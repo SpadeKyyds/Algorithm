813274b4d4a06d097cc781b546cae45de97ac9b4
SpadeKyyds <2190532541@qq.com>
2022-12-06 19:49:38 +0800

    2022/12/06 first commit

---
+++ 5双指针法/8环形链表_142.cpp
@@ -0,0 +1,80 @@
+/**
+ * 难度：中等
+ * 给定一个链表的头节点  head ，返回链表开始入环的第一个节点。 如果链表无环，则返回 null。
+
+    如果链表中有某个节点，可以通过连续跟踪 next 指针再次到达，则链表中存在环。 为了表示给定链表中的环，评测系统内部使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。如果 pos 是 -1，则在该链表中没有环。注意：pos 不作为参数进行传递，仅仅是为了标识链表的实际情况。
+
+    不允许修改 链表。
+
+     
+
+    示例 1：
+
+
+
+    输入：head = [3,2,0,-4], pos = 1
+    输出：返回索引为 1 的链表节点
+    解释：链表中有一个环，其尾部连接到第二个节点。
+    示例 2：
+
+
+
+    输入：head = [1,2], pos = 0
+    输出：返回索引为 0 的链表节点
+    解释：链表中有一个环，其尾部连接到第一个节点。
+    示例 3：
+
+
+
+    输入：head = [1], pos = -1
+    输出：返回 null
+    解释：链表中没有环。
+     
+
+    提示：
+
+    链表中节点的数目范围在范围 [0, 104] 内
+    -105 <= Node.val <= 105
+    pos 的值为 -1 或者链表中的一个有效索引
+
+    来源：力扣（LeetCode）
+    链接：https://leetcode.cn/problems/linked-list-cycle-ii
+    著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+using namespace std;
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+// 自己凭借记忆写出 通过！ 自己已经吸收的版本 但是在代码的整体简洁方面还是要借鉴卡尔的
+class Solution {
+public:
+    ListNode* detectCycle(ListNode* head) {
+        if (head == nullptr || head->next == nullptr) return nullptr;
+        else {
+            ListNode* slow = head;
+            ListNode* fast = head;
+            do {
+                fast = fast->next->next;
+                slow = slow->next;
+            } while (fast != nullptr && fast->next != nullptr && fast != slow);
+            if (fast != slow) return nullptr;
+            else {
+                ListNode* ptr = head;
+                while (ptr != slow) {
+                    ptr = ptr->next;
+                    slow = slow->next;
+                }
+                return ptr;
+            }
+        }
+    }
+};
